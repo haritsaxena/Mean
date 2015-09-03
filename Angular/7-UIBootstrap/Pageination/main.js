143a5742baca5bfdc36a9c8b966fe0075828419d
Harit Saxena <Harit.Saxena@gmail.com>
2015-09-03 19:15:27 +0530

    added uibootstrap

---
+++ Angular/7-UIBootstrap/Pageination/main.js
@@ -0,0 +1,20 @@
+'use strict';
+
+angular.module('ui.bootstrap.demo', ['ngAnimate', 'ui.bootstrap']);
+
+angular.module('ui.bootstrap.demo').controller('PaginationDemoCtrl', function ($scope,$log) {
+	$scope.totalItems = 84;
+	$scope.currentPage = 4;
+
+	$scope.setPage = function (pageNo) {
+	$scope.currentPage = pageNo;
+	};
+
+	$scope.pageChanged = function() {
+	$log.log('Page changed to: ' + $scope.currentPage);
+	};
+
+	$scope.maxSize = 5;
+	$scope.bigTotalItems = 175;
+	$scope.bigCurrentPage = 1;
+});
